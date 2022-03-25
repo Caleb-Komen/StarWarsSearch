53dd5e26227e2345d5ac494a4500cd35b6bb7771
Caleb-Komen <calebkipyatich@gmail.com>
2022-03-25 23:58:14 +0300

    Script for running unit tests.

---
+++ testsScripts/unit_tests.sh
@@ -0,0 +1,25 @@
+print_red(){
+  printf "\e[1;31m$1\e[0m"
+}
+
+print_green(){
+    printf "\e[1;32m$1\e[0m"
+}
+
+print_yellow(){
+    printf "\e[1;33m$1\e[0m"
+}
+
+print_blue(){
+    printf "\e[1;34m$1\e[0m"
+}
+
+print_blue "\nStarting..."
+
+cd "/home/caleb/AndroidStudioProjects/StarWarsSearch/"
+
+print_yellow "\n\nRunning unit tests...\n"
+
+./gradlew data:test
+
+print_green "\nUnit tests COMPLETE.\n"
