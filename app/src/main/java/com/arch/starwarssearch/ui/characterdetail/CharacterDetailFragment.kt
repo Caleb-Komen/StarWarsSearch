a7c6c95709b4a1f30c1cd2e6acdf9d76c9604020
Caleb-Komen <calebkipyatich@gmail.com>
2022-04-10 22:27:44 +0300

    Design character detail layout.

---
+++ app/src/main/java/com/arch/starwarssearch/ui/characterdetail/CharacterDetailFragment.kt
@@ -0,0 +1,20 @@
+package com.arch.starwarssearch.ui.characterdetail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.arch.starwarssearch.R
+
+class CharacterDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_character_detail, container, false)
+    }
+
+}
