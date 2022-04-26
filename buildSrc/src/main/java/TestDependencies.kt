45ad695c014aa6a717134a99bb959c871cf7cec2
Caleb-Komen <calebkipyatich@gmail.com>
2022-04-26 23:55:57 +0300

    Rearrange dependencies.

--- buildSrc/src/main/java/TestDependencies.kt
+++ buildSrc/src/main/java/TestDependencies.kt
@@ -1,7 +1,5 @@
 object TestDependencies {
     const val junit4 = "junit:junit:${Versions.junit}"
-    const val testJunit = "androidx.test.ext:junit:1.1.3:${Versions.testJunit}"
-    const val espresso = "androidx.test.espresso:espresso-core:${Versions.espresso}"
     const val mockWebServer = "com.squareup.okhttp3:mockwebserver:${Versions.mockWebServer}"
     const val truth = "com.google.truth:truth:${Versions.truth}"
     const val coroutinesTest = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.coroutines}"
