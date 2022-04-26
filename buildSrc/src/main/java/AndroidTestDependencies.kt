db6e44aa8213143e206f8eb18437d541d6a628b4
Caleb-Komen <calebkipyatich@gmail.com>
2022-04-26 23:55:57 +0300

    Rearrange dependencies.

---
+++ buildSrc/src/main/java/AndroidTestDependencies.kt
@@ -0,0 +1,4 @@
+object AndroidTestDependencies {
+    const val testJunit = "androidx.test.ext:junit:1.1.3:${Versions.testJunit}"
+    const val espresso = "androidx.test.espresso:espresso-core:${Versions.espresso}"
+}
