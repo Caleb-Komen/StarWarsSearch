2a38186ff0fae9a2ee994b8de2a140ab68c9579c
Caleb-Komen <calebkipyatich@gmail.com>
2022-05-24 14:37:28 +0300

    Rename file

--- app/src/main/java/com/arch/starwarssearch/StarWarsActivity.kt
+++ app/src/main/java/com/arch/starwarssearch/StarWarsActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class StarWarsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
